d30f86a930038a5a26c3cedf20d5e49b526350b7
Jhimi Talukder <talukderjhimi@gmail.com>
2023-05-17 21:23:08 +0600

    setup

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <div>
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <button className="btn">Button</button>
+<button className="btn btn-primary">Button</button>
+<button className="btn btn-secondary">Button</button>
+<button className="btn btn-accent">Button</button>
+<button className="btn btn-ghost">Button</button>
+<button className="btn btn-link">Button</button>
+     </div>
+    </>
+  )
+}
+
+export default App
