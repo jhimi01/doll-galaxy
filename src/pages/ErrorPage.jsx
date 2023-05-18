eff8b6cbe52ba988f91aecf9e9d8dc6fc1b703fb
Jhimi Talukder <talukderjhimi@gmail.com>
2023-05-18 19:38:47 +0600

    error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,15 +1,13 @@
-// import Lottie from "react-lottie";
 import { Link } from "react-router-dom";
-// import ErrorAnimation from '../assets/Error.json'
+import ErrorAnimation from '../assets/Error.json'
+import Lottie from "lottie-react";
 
 const ErrorPage = () => {
     return (
         <div className="flex flex-col items-center justify-center h-screen">
-        {/* {ErrorAnimation && (
-  <Lottie style={{ height: 300 }} animationData={ErrorAnimation} />
-)} */}
+   <Lottie style={{height: 300}} animationData={ErrorAnimation} />
 <h1 className="text-3xl font-bold mb-4">Oops! Something went wrong.</h1>
-      <p className="text-lg text-gray-300 mb-4">
+      <p className="text-lg text-gray-500 font-bold mb-4">
         We are sorry, but the page you were looking for could not be found.
       </p>
 
