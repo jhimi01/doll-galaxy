1093a8e162e24ade11942b15b12dcc83843fd324
Jhimi Talukder <talukderjhimi@gmail.com>
2023-05-21 11:46:44 +0600

    modify

--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -12,19 +12,6 @@ const MyToys = () => {
   const [control, setControl] = useState(false);
 
 
-  // dropdown----------------
-  const handleOptionChange = (e) => {
-    setSelectedOption(e.target.value);
-  };
-  const handleSort = () => {
-    // Add your sorting logic here based on the selectedOption
-    if (selectedOption === 'lowToHigh') {
-      console.log('Sorting items from low to high');
-    } else if (selectedOption === 'highToLow') {
-      console.log('Sorting items from high to low');
-    }
-  };
-// ----------------
 
 const url = `https://toy-galaxy-server-plum.vercel.app/user/data?email=${user.email}`
 useEffect(()=>{
@@ -36,6 +23,30 @@ useEffect(()=>{
 // console.log(url)
 
 
+  // dropdown----------------
+  const handleOptionChange = (e) => {
+    setSelectedOption(e.target.value);
+  };
+  const handleSort = () => {
+    // Add your sorting logic here based on the selectedOption
+    if (selectedOption === 'lowToHigh') {
+      fetch(`https://toy-galaxy-server-plum.vercel.app/user/sort/low?email=${user.email}`)
+      .then(res => res.json())
+      .then(data => setMytoys(data))
+     
+    }
+
+   
+
+     else if (selectedOption === 'highToLow') {
+      fetch(`https://toy-galaxy-server-plum.vercel.app/user/sort/high?email=${user.email}`)
+      .then(res => res.json())
+      .then(data => setMytoys(data))
+     
+    }
+  };
+  console.log(mytoys)
+// ----------------
 
 
 
@@ -44,9 +55,10 @@ useEffect(()=>{
     <div className='w-full md:w-5/6 px-3 md:px-0 mx-auto '>
     {/* sorting button */}
        <div className="flex items-center justify-center mt-10">
-      <div className="flex items-center space-x-4">
+      <div className="flex flex-col items-center space-x-4">
         <h2 className="text-xl font-bold">Sort Dropdown</h2>
-        <select
+       <div className='mt-2'>
+       <select
           value={selectedOption}
           onChange={handleOptionChange}
           className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
@@ -57,10 +69,11 @@ useEffect(()=>{
         </select>
         <button
           onClick={handleSort}
-          className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className="px-4 ml-3 py-2 text-white bg-secondary rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         >
           Sort
         </button>
+       </div>
       </div>
     </div>
 
